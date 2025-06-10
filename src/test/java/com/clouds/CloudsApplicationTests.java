d5afc02beef9c6531ffa44286f94a0ae5d610af0
sumit-the-coder <biswas.sumitava@gmail.com>
2025-06-10 23:19:57 +0200

    Add logging

--- src/test/java/com/clouds/CloudsApplicationTests.java
+++ src/test/java/com/clouds/CloudsApplicationTests.java
@@ -3,11 +3,16 @@ package com.clouds;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 @SpringBootTest
 class CloudsApplicationTests {
-
+	public static final Logger logger = (Logger) LogManager.getLogger(CloudsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info(CloudsApplicationTests.class.getName());
 	}
 
 }
